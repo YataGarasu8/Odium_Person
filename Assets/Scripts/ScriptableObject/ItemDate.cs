a8ce6e3e75b2ef06aee5080d5fa62f2ca7210990
YataGarasu8 <zmffldjdnld12@naver.com>
2025-03-20 17:12:32 +0900

    [Feat]상세 폴더 분할

---
+++ Assets/Scripts/ScriptableObject/ItemDate.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemDate", menuName ="Scriptable Object/Itemdate")]
+public class ItemDate : ScriptableObject
+{
+  
+}
